c09a413ff01f73a819a0f9ef6511138736e26832
Hyo298 <zxcvb7587@naver.com>
2024-12-11 10:30:16 +0900

    project: 프로젝트 생성

---
+++ src/test/java/com/hyo/webmodules/HyoWebModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyo.webmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyoWebModulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
